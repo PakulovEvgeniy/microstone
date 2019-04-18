5e4d279407dabd6b0075c54922e53c3176906aa2
PakulovEvgeniy <info@microstone.ru>
2019-04-18 19:28:36 +0900

    555

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -10,6 +10,7 @@ class AppController extends Controller
      public function get(Request $request) {
         $ssr = JSRender::render($request->path(), []);
         //$rend = $this->render($request->path()); 
+        //$ssr = phpinfo();
         return view('app', ['ssr' => $ssr]);
     }
 }
